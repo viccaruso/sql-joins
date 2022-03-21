74ab69bbd567c67c0763d9d1f105abd1964bf6e5
Victor Caruso <vic.caruso@gmail.com>
2022-03-21 15:25:32 -0700

    exercise 3 completed

--- exercise_3/exercise-3.sql
+++ exercise_3/exercise-3.sql
@@ -1,3 +1,16 @@
 -- find all film titles with the category Action
 -- use a LEFT JOIN FROM film onto film_category
 -- use LEFT JOIN FROM film_categor onto category
+SELECT
+	title
+FROM 
+	category
+LEFT JOIN
+	film_category
+ON 
+	category.category_id = film_category.category_id
+LEFT JOIN
+	film
+ON film_category.film_id = film.film_id
+WHERE 
+	category.name = 'Action';

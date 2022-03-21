369e040b36e40e132de80303642129ee7f5bcaee
Victor Caruso <vic.caruso@gmail.com>
2022-03-21 15:55:29 -0700

    exercise 6 completed

--- exercise_6/exercise-6.sql
+++ exercise_6/exercise-6.sql
@@ -1,2 +1,20 @@
 -- find all film titles rented out by customers with the first_name Roberta
 -- Similar to exercise 5
+SELECT
+	title
+FROM
+	customer
+LEFT JOIN
+	rental
+ON 
+	customer.customer_id = rental.customer_id
+LEFT JOIN
+	inventory
+ON
+	inventory.inventory_id = rental.inventory_id
+LEFT JOIN
+	film
+ON 
+	film.film_id = inventory.film_id
+WHERE
+	customer.first_name = 'Roberta';
